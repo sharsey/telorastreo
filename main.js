aab67bc46b9711c1a1e8f0326db9e0ff6a115fc3
S <sharsey.code@gmail.com>
2020-10-28 01:48:48 -0500

    Add prototype

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const burgermenu = document.getElementById('burger-menu');
 const navmenu = document.getElementById('navbar');
-console.log("Im working")
 burgermenu.addEventListener('click', () => {
   navmenu.classList.toggle('show');
   
