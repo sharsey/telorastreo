1e46df7e63d8fc9adceab3700b2d05b2b6746e2c
S <sharsey.code@gmail.com>
2020-11-05 16:41:08 -0500

    Retry changes

--- main.js
+++
@@ -1,6 +0,0 @@
-const burgermenu = document.getElementById('burger-menu');
-const navmenu = document.getElementById('navbar');
-burgermenu.addEventListener('click', () => {
-  navmenu.classList.toggle('show');
-  
-});
