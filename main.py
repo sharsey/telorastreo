2272ae02d02517afa044c8dd7f3230f626e4feff
S <sharsey.code@gmail.com>
2020-11-19 00:23:14 -0500

    error pages

--- main.py
+++ main.py
@@ -15,5 +15,14 @@ def contact():
 def information():
     return render_template('information.html')
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html', error=error)
+
 
 
