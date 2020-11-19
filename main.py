03c6bec2c0e6e4b689aaca7fd4fa5785b8db08bc
root <root@mytraccar.c.traccar-280707.internal>
2020-11-19 06:16:20 +0000

    Cambios puerto 80

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=80)
+
+
 
 
 @app.route('/')
@@ -25,4 +29,5 @@ def internal_error(error):
     return render_template('500.html', error=error)
 
 
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
